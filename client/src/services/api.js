403a68962777b3b93312678c4e1846fa87b6d463
Nentur Varshini <varshininentur@gmail.com>
2026-07-09 18:28:57 -0700

    Updated backend api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://rectify-backend-90b3.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
